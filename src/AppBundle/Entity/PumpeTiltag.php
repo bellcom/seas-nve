ce005d0245d0ae2a6daca5693edeb7ff6694d798
Ture Gjørup <ture@ignatz.dk>
2015-02-17 15:45:11 +0100

    AAPLUS-50: Setup base class for Tiltag, added pumpetiltag

---
+++ src/AppBundle/Entity/PumpeTiltag.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\DiscriminatorMap;
+use Doctrine\ORM\Mapping\DiscriminatorColumn;
+use Doctrine\ORM\Mapping\InheritanceType;
+
+/**
+ * Tiltag
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\PumpeTiltagRepository")
+ */
+class PumpeTiltag extends Tiltag
+{
+
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+
+  /**
+   * Get id
+   *
+   * @return integer
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+}
--- src/AppBundle/Entity/Rapport.php
+++ src/AppBundle/Entity/Rapport.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\JoinColumn;
 
@@ -25,9 +26,15 @@ class Rapport
 
     /**
      * @ManyToOne(targetEntity="Bygning", inversedBy="rapporter")
-     * @JoinColumn(name="rapport_id", referencedColumnName="id")
+     * @JoinColumn(name="bygning_id", referencedColumnName="id")
      **/
     private $bygning;
+
+
+    /**
+     * @OneToMany(targetEntity="Tiltag", mappedBy="rapport")
+     */
+    private $tiltag;
 
     /**
      * @var string
@@ -100,4 +107,67 @@ class Rapport
         return $this->datering;
     }
 
+
+    /**
+     * Set bygning
+     *
+     * @param \AppBundle\Entity\Bygning $bygning
+     * @return Rapport
+     */
+    public function setBygning(\AppBundle\Entity\Bygning $bygning = null)
+    {
+        $this->bygning = $bygning;
+
+        return $this;
+    }
+
+    /**
+     * Get bygning
+     *
+     * @return \AppBundle\Entity\Bygning 
+     */
+    public function getBygning()
+    {
+        return $this->bygning;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->tiltag = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add tiltag
+     *
+     * @param \AppBundle\Entity\Tiltag $tiltag
+     * @return Rapport
+     */
+    public function addTiltag(\AppBundle\Entity\Tiltag $tiltag)
+    {
+        $this->tiltag[] = $tiltag;
+
+        return $this;
+    }
+
+    /**
+     * Remove tiltag
+     *
+     * @param \AppBundle\Entity\Tiltag $tiltag
+     */
+    public function removeTiltag(\AppBundle\Entity\Tiltag $tiltag)
+    {
+        $this->tiltag->removeElement($tiltag);
+    }
+
+    /**
+     * Get tiltag
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getTiltag()
+    {
+        return $this->tiltag;
+    }
 }
