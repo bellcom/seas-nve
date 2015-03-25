f65d886d9012932368575070c03c52337db3f834
rimi-itk <rimi@aarhus.dk>
2015-03-25 13:55:37 +0100

    Add BelysningTiltag entity

---
+++ src/AppBundle/Entity/BelysningTiltag.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * @TODO: Missing description.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BelysningTiltag.
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\BelysningTiltagRepository")
+ */
+class BelysningTiltag extends Tiltag {}
