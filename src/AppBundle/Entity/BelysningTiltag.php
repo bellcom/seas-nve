1d8f8c2b7ab3ce6933aa35509821469cfc5cb014
Ture Gjørup <ture@ignatz.dk>
2015-03-26 12:22:20 +0100

    Set default title on 'Belysningstiltag'

--- src/AppBundle/Entity/BelysningTiltag.php
+++ src/AppBundle/Entity/BelysningTiltag.php
@@ -14,4 +14,15 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="AppBundle\Entity\BelysningTiltagRepository")
  */
-class BelysningTiltag extends Tiltag {}
+class BelysningTiltag extends Tiltag {
+
+  /**
+   * Constructor
+   */
+  public function __construct() {
+    $this->pumpedetails = new \Doctrine\Common\Collections\ArrayCollection();
+
+    // @Todo: Find af way to use the translations system or move this to some place else....
+    $this->setTitle('Belysningstiltag');
+  }
+}
