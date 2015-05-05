8a2ebd393b03ae06d269f7130f1d62627e027348
rimi-itk <rimi@aarhus.dk>
2015-05-05 16:33:30 +0200

    Fix calculations

--- src/AppBundle/Entity/TiltagDetail.php
+++ src/AppBundle/Entity/TiltagDetail.php
@@ -126,7 +126,7 @@ abstract class TiltagDetail {
    * @JoinColumn(name="tiltag_id", referencedColumnName="id")
    * @JMS\Type("AppBundle\Entity\Tiltag")
    **/
-  private $tiltag;
+  protected $tiltag;
 
   /**
    * Set tiltag
@@ -212,6 +212,15 @@ abstract class TiltagDetail {
   }
 
   /**
+   * Get the rapport (convenience method)
+   *
+   * @return Rapport
+   */
+  public function getRapport() {
+    return $this->getTiltag()->getRapport();
+  }
+
+  /**
    * Handle uploads.
    * @param $manager
    */
@@ -232,18 +241,85 @@ abstract class TiltagDetail {
    */
   public function compute() {}
 
-  protected function fordelbesparelse($a, $b, $c) {
+  protected function fordelbesparelse($BesparKwh, $Kilde, $typen) {
     return 0;
+
+    /*
+Public Function FordelBesparelse(BesparKwh As Single, Kilde As String, typen As String)
+
+    Dim lastRow As Integer
+    Dim y As Integer
+    Dim t As Integer
+
+    With Worksheets("2.Forsyning")
+        lastRow = Worksheets("2.Forsyning").Range("a3").Value 'sidste række i øverste tabel
+    End With
+
+    For y = 14 To lastRow
+
+        If Worksheets("2.Forsyning").Range("a" & y) = Kilde Then
+            If typen = "EL" Or typen = "VARME" Or typen = "PRIORITEREL" Or typen = "PRIORITERVARME" Then 'tjekker at der er tale om alm. el og varme og ikke "konverteringvarme" hvor det gamle forsyningsværks værdier skal bruges
+                If Worksheets("2.Forsyning").Range("ai" & y) <> "" Then 'der er beregnet konvertering
+                    t = Worksheets("2.Forsyning").Range("ai" & y).Value 'række for konvertering indtastes
+
+                    If Worksheets("2.Forsyning").Range("a" & t) <> "" Then 'konverteringen er tilvalgt
+                        If typen = "EL" Or typen = "VARME" Then
+                            If Worksheets("2.Forsyning").Range("aj" & t) = 1 Then 'konvertering prioriteres forud for øvrige tiltag
+                                y = t 'denne funktion bypasses hvis de er er tale om KONVERTERINGVARME eller KONVERTERINGEL, hvor det eksisterende værks værdier skal anvendes.
+                                'MsgBox ("y er " & y)
+                            End If
+
+                        ElseIf typen = "PRIORITEREL" Then
+                        y = t 'prioriterel og varme er altid efter nye forsyningskildes effektivitet, uanset om prioritering er 1 eller 2
+                        typen = "EL"
+                        ElseIf typen = "PRIORITERVARME" Then
+                        y = t
+                        typen = "VARME"
+                        'MsgBox ("prioritering virker")
+                        End If
+                    End If
+                End If
+
+            ElseIf typen = "KONVERTERINGVARME" Then 'dvs. regner altid med gamle værks effektivitet
+                typen = "VARME"
+            ElseIf typen = "KONVERTERINGEL" Then 'dvs. regner altid med gamle værks effektivitet
+                typen = "EL"
+            End If
+
+            If typen = "EL" Then
+                 BesparKwh = BesparKwh * Worksheets("2.Forsyning").Range("ah" & y)
+            End If
+
+            If typen = "VARME" Then
+                BesparKwh = BesparKwh * Worksheets("2.Forsyning").Range("ag" & y)
+            End If
+
+            y = lastRow 'såfremt der forsyningskilden er fundet, skal efterfølgende rækker ikke kontrolleres
+        End If
+
+    Next y
+
+    If typen = "VARME" Then
+        FordelBesparelse = BesparKwh
+    End If
+
+    If typen = "EL" Then
+        FordelBesparelse = BesparKwh
+    End If
+
+End Function
+*/
   }
 
   protected function nvPTO2($Invest, $BesparKwhVarme, $BesparKwhEl, $Besparm3Vand, $DogV, $Straf, $Levetid, $FaktorReInvest, $SalgAfEnergibesparelse) {
-    $Kalkulationsrente = 0.0292; // Worksheets("1.Tiltagslisterådgiver").Range("ai23").Value;
-    $Inflationsfaktor = 13.8639998427607; // Worksheets("1.Tiltagslisterådgiver").Range("ai26");
-    $Inflation = 0.019; // Worksheets("1.Tiltagslisterådgiver").Range("ak23");
-    $Lobetid = 15; // Worksheets("1.Tiltagslisterådgiver").Range("an23");
-    $Elfaktor = 23.5852836416293; // Worksheets("1.TiltagslisteRådgiver").Range("ah25"); // 'tilbagediskonterede faktorer for energi-priser over 15 år
-    $Varmefaktor = 5.66155209590081; // Worksheets("1.TiltagslisteRådgiver").Range("ah24");
-    $Vandfaktor = 566.621673573629; // Worksheets("1.TiltagslisteRådgiver").Range("ai27");
+    $rapport = $this->tiltag->getRapport();
+    $Kalkulationsrente = $rapport->getKalkulationsrente(); // Worksheets("1.Tiltagslisterådgiver").Range("ai23").Value;
+    $Inflationsfaktor = $rapport->getInflationsfaktor(); // Worksheets("1.Tiltagslisterådgiver").Range("ai26");
+    $Inflation = $rapport->getInflation(); // Worksheets("1.Tiltagslisterådgiver").Range("ak23");
+    $Lobetid  = $rapport->getLobetid(); // Worksheets("1.Tiltagslisterådgiver").Range("an23");
+    $Elfaktor = $rapport->getElfaktor(); // Worksheets("1.TiltagslisteRådgiver").Range("ah25"); // 'tilbagediskonterede faktorer for energi-priser over 15 år
+    $Varmefaktor = $rapport->getVarmefaktor(); // Worksheets("1.TiltagslisteRådgiver").Range("ah24");
+    $Vandfaktor = $rapport->getVandfaktor(); // Worksheets("1.TiltagslisteRådgiver").Range("ai27");
 
     $Reinvest = 0;
     $AntalReinvest = 0;
@@ -251,30 +327,82 @@ abstract class TiltagDetail {
 
     if ($Levetid > 0) {
       if ($Levetid < $Lobetid) { // 'reinvestering foretages
-          $AntalReinvest = floor($Lobetid / $Levetid);
+        $AntalReinvest = floor($Lobetid / $Levetid);
 
-          if ($AntalReinvest == 1) {
-            $Reinvest = ($Invest * $FaktorReInvest * (1 + $Inflation) ^ ($Levetid + 1)) / ((1 + $Kalkulationsrente) ^ ($Levetid + 1));
-          }
-          elseif ($AntalReinvest > 1) { // 'kan evt. forbedres til mere statisk formel aht. beregningshastigheden
-            for ($x = 1; $x <= $AntalReinvest; $x++) {
-              $Reinvest = $Reinvest + ($Invest * $FaktorReInvest * (1 + $Inflation) ^ (($Levetid * $x) + 1)) / ((1 + $Kalkulationsrente) ^ (($Levetid * $x) + 1));
-            }
+        if ($AntalReinvest == 1) {
+          $Reinvest = ($Invest * $FaktorReInvest * pow(1 + $Inflation, $Levetid + 1)) / pow(1 + $Kalkulationsrente, $Levetid + 1);
+        }
+        elseif ($AntalReinvest > 1) { // 'kan evt. forbedres til mere statisk formel aht. beregningshastigheden
+          for ($x = 1; $x <= $AntalReinvest; $x++) {
+            $Reinvest = $Reinvest + ($Invest * $FaktorReInvest * pow(1 + $Inflation, $Levetid * $x + 1)) / (pow(1 + $Kalkulationsrente, $Levetid * $x + 1));
           }
         }
-
+      }
 
       if ($Levetid > $Lobetid) { // 'ingen reinvesteringer
-        $Scrapvaerdi = (1 - ($Lobetid / $Levetid)) * $Invest * (1 + $Inflation) ^ $Lobetid;
+        $Scrapvaerdi = (1 - ($Lobetid / $Levetid)) * $Invest * pow(1 + $Inflation, $Lobetid);
       }
       elseif ($Lobetid - $AntalReinvest * $Levetid == 0) {
         $Scrapvaerdi = 0;
       } else {
-        $Scrapvaerdi = (1 - ($Lobetid - $AntalReinvest * $Levetid) / $Levetid) * $Invest * $FaktorReInvest * (1 + $Inflation) ^ $Lobetid;
+        $Scrapvaerdi = (1 - ($Lobetid - $AntalReinvest * $Levetid) / $Levetid) * $Invest * $FaktorReInvest * pow(1 + $Inflation, $Lobetid);
       }
     }
 
-    return ((-$Invest + $SalgAfEnergibesparelse) / (1 + $Kalkulationsrente) ^ 1) + $BesparKwhVarme * $Varmefaktor + $BesparKwhEl * $Elfaktor + $Besparm3Vand * $Vandfaktor + ($Scrapvaerdi / (1 + $Kalkulationsrente) ^ $Lobetid) + ($DogV + $Straf) * $Inflationsfaktor - $Reinvest;
+    return ((-$Invest + $SalgAfEnergibesparelse) / (1 + $Kalkulationsrente)) + $BesparKwhVarme * $Varmefaktor + $BesparKwhEl * $Elfaktor + $Besparm3Vand * $Vandfaktor + ($Scrapvaerdi / pow(1 + $Kalkulationsrente, $Lobetid)) + ($DogV + $Straf) * $Inflationsfaktor - $Reinvest;
   }
+
+  /*
+Public Function NvPTO2(Invest As Single, BesparKwhVarme As Single, BesparKwhEl As Single, Besparm3Vand As Single, DogV As Single, Straf As Single, Levetid As Single, FaktorReInvest As Single, SalgAfEnergibesparelse)
+Dim Kalkulationsrente As Single
+Dim Inflationsfaktor As Single
+Dim Inflation As Single
+Dim Lobetid As Integer
+Dim AntalReinvest As Integer
+Dim Elfaktor As Single
+Dim Varmefaktor As Single
+Dim Vandfaktor As Single
+Dim Scrapvaerdi As Long
+Dim Reinvest As Single
+Dim x As Integer
+
+Kalkulationsrente = Worksheets("1.Tiltagslisterådgiver").Range("ai23").Value
+Inflationsfaktor = Worksheets("1.Tiltagslisterådgiver").Range("ai26")
+Inflation = Worksheets("1.Tiltagslisterådgiver").Range("ak23")
+Lobetid = Worksheets("1.Tiltagslisterådgiver").Range("an23")
+Elfaktor = Worksheets("1.TiltagslisteRådgiver").Range("ah25") 'tilbagediskonterede faktorer for energi-priser over 15 år
+Varmefaktor = Worksheets("1.TiltagslisteRådgiver").Range("ah24")
+Vandfaktor = Worksheets("1.TiltagslisteRådgiver").Range("ai27")
+
+Reinvest = 0
+AntalReinvest = 0
+
+If Levetid > 0 Then
+If Levetid < Lobetid Then 'reinvestering foretages
+    AntalReinvest = Application.RoundDown(Lobetid / Levetid, 0)
+
+    If AntalReinvest = 1 Then
+        Reinvest = (Invest * FaktorReInvest * (1 + Inflation) ^ (Levetid + 1)) / ((1 + Kalkulationsrente) ^ (Levetid + 1))
+    ElseIf AntalReinvest > 1 Then 'kan evt. forbedres til mere statisk formel aht. beregningshastigheden
+        For x = 1 To AntalReinvest
+            Reinvest = Reinvest + (Invest * FaktorReInvest * (1 + Inflation) ^ ((Levetid * x) + 1)) / ((1 + Kalkulationsrente) ^ ((Levetid * x) + 1))
+        Next
+    End If
+
+End If
+
+
+If Levetid > Lobetid Then 'ingen reinvesteringer
+    Scrapvaerdi = (1 - (Lobetid / Levetid)) * Invest * (1 + Inflation) ^ Lobetid
+ElseIf (Lobetid - AntalReinvest * Levetid) = 0 Then
+   Scrapvaerdi = 0
+Else
+    Scrapvaerdi = (1 - (Lobetid - AntalReinvest * Levetid) / Levetid) * Invest * FaktorReInvest * (1 + Inflation) ^ Lobetid
+End If
+End If
+
+    NvPTO2 = ((-Invest + SalgAfEnergibesparelse) / (1 + Kalkulationsrente) ^ 1) + BesparKwhVarme * Varmefaktor + BesparKwhEl * Elfaktor + Besparm3Vand * Vandfaktor + (Scrapvaerdi / (1 + Kalkulationsrente) ^ Lobetid) + (DogV + Straf) * Inflationsfaktor - Reinvest
+End Function
+*/
 
 }
