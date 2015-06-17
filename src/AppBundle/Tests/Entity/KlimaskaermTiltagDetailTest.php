2bced23a3ea2ffbf95eb21123e5882d8f2c16b3b
rimi-itk <rimi@aarhus.dk>
2015-06-17 10:51:50 +0200

    Issue #AAPLUS-123 by rimi-itk: Updated unit test for KlimaskaermTiltagDetail

--- src/AppBundle/Tests/Entity/KlimaskaermTiltagDetailTest.php
+++ src/AppBundle/Tests/Entity/KlimaskaermTiltagDetailTest.php
@@ -1,11 +1,20 @@
 <?php
 namespace AppBundle\Tests\Entity;
 
-use AppBundle\Entity\Rapport;
+use AppBundle\Entity\Klimaskaerm;
 use AppBundle\Entity\KlimaskaermTiltag;
 use AppBundle\Entity\KlimaskaermTiltagDetail;
 
 class KlimaskaermTiltagDetailTest extends TiltagDetailTestCase {
   // protected $allowedDeviance = 0.001;
+
+  public function loadProperties(array $properties) {
+    $properties['klimaskaerm'] = $this->getKlimaskaerm($properties['klimaskaerm']);
+    return $properties;
+  }
+
+  private function getKlimaskaerm(array $properties = null) {
+    return $properties ? $this->loadEntity(new Klimaskaerm(), $properties) : null;
+  }
 
 }
