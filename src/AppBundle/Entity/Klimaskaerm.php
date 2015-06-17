da34ab60e617d27e4276370f4f09dd275651cc2f
rimi-itk <rimi@aarhus.dk>
2015-06-17 10:41:11 +0200

    Issue #AAPLUS-123 by rimi-itk: Added entity for Klimaskaerm

---
+++ src/AppBundle/Entity/Klimaskaerm.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * @file
+ * @TODO: Missing description.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Klimaskaerm
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\KlimaskaermRepository")
+ */
+class Klimaskaerm {
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="post", type="string", length=255)
+   */
+  private $post;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="klimaskaerm", type="string", length=255, nullable=true)
+   */
+  private $klimaskaerm;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="arbejdeOmfang", type="string", length=255, nullable=true)
+   */
+  private $arbejdeOmfang;
+
+  /**
+   * @var float
+   *
+   * @ORM\Column(name="enhedsprisEksklMoms", type="decimal", scale=2)
+   */
+  private $enhedsprisEksklMoms;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="enhed", type="string", length=255)
+   */
+  private $enhed;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="noter", type="text", nullable=true)
+   */
+  private $noter;
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function setPost($post) {
+    $this->post = $post;
+
+    return $this;
+  }
+
+  public function getPost() {
+    return $this->post;
+  }
+
+  public function setKlimaskaerm($klimaskaerm) {
+    $this->klimaskaerm = $klimaskaerm;
+
+    return $this;
+  }
+
+  public function getKlimaskaerm() {
+    return $this->klimaskaerm;
+  }
+
+  public function setArbejdeOmfang($arbejdeOmfang) {
+    $this->arbejdeOmfang = $arbejdeOmfang;
+
+    return $this;
+  }
+
+  public function getArbejdeOmfang() {
+    return $this->arbejdeOmfang;
+  }
+
+  public function setEnhedsprisEksklMoms($enhedsprisEksklMoms) {
+    $this->enhedsprisEksklMoms = $enhedsprisEksklMoms;
+
+    return $this;
+  }
+
+  public function getEnhedsprisEksklMoms() {
+    return $this->enhedsprisEksklMoms;
+  }
+
+  public function setEnhed($enhed) {
+    $this->enhed = $enhed;
+
+    return $this;
+  }
+
+  public function getEnhed() {
+    return $this->enhed;
+  }
+
+  public function setNoter($noter) {
+    $this->noter = $noter;
+
+    return $this;
+  }
+
+  public function getNoter() {
+    return $this->noter;
+  }
+
+  public function __toString() {
+    return $this->klimaskaerm ? $this->post . ', ' . $this->klimaskaerm : $this->post;
+  }
+
+}
