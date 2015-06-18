a2c6e22186cd590eb15529c827c76a7fb94aa3de
rimi-itk <rimi@aarhus.dk>
2015-06-18 11:16:08 +0200

    Issue #AAPLUS-122 by rimi-itk: Load Solcelle for SolcelleTiltagDetail in unit test

--- src/AppBundle/Tests/Entity/SolcelleTiltagDetailTest.php
+++ src/AppBundle/Tests/Entity/SolcelleTiltagDetailTest.php
@@ -1,9 +1,19 @@
 <?php
 namespace AppBundle\Tests\Entity;
 
-use AppBundle\Entity\Rapport;
+use AppBundle\Entity\Solcelle;
 
 class SolcelleTiltagDetailTest extends TiltagDetailTestCase {
   // protected $allowedDeviance = 0.001;
+
+  protected function loadProperties(array $properties) {
+    $properties['solcelle'] = $this->getSolcelle($properties['solcelle']);
+
+    return $properties;
+  }
+
+  private function getSolcelle(array $properties = null) {
+    return $properties ? $this->loadEntity(new Solcelle(), $properties) : null;
+  }
 
 }
