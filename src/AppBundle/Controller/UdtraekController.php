7900d8c96c2592d2ab65ee5d853fa7952d3b72c3
Troels Ugilt Jensen <tujensen@gmail.com>
2016-01-26 14:34:34 +0100

    Issue #AAPLUS-298 by tuj: Added Udtraek page

---
+++ src/AppBundle/Controller/UdtraekController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @file
+ * @TODO: Missing description.
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\DataExport\ExcelExport;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Yavin\Symfony\Controller\InitControllerInterface;
+
+/**
+ * Udtraek controller.
+ *
+ * @Route("/udtraek")
+ */
+class UdtraekController extends BaseController implements InitControllerInterface {
+
+  protected $breadcrumbs;
+
+  public function init(Request $request) {
+    parent::init($request);
+    $this->breadcrumbs->addItem('Eksport', $this->generateUrl('eksport'));
+  }
+
+  /**
+   * Get udtraek page.
+   *
+   * @Route("/", name="udtraek")
+   * @Method("GET")
+   * @Template()
+   */
+  public function indexAction(Request $request) {
+
+    return $this->render('AppBundle:Udtraek:index.html.twig', array());
+  }
+}
