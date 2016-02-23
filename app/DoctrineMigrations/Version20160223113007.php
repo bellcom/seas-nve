b2965238fddbb7a618b8bf1ff569c9859ed5865c
Troels Ugilt Jensen <tujensen@gmail.com>
2016-02-23 12:17:21 +0100

    Issue #AAPLUS-427 by tuj: Cleaned up migrations

--- app/DoctrineMigrations/Version20160223113007.php
+++ app/DoctrineMigrations/Version20160223113007.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160222142823 extends AbstractMigration
+class Version20160223113007 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -20,6 +20,8 @@ class Version20160222142823 extends AbstractMigration
 
         $this->addSql('ALTER TABLE Forsyningsvaerk ADD pris2009 NUMERIC(10, 4) DEFAULT NULL, ADD pris2010 NUMERIC(10, 4) DEFAULT NULL, ADD pris2011 NUMERIC(10, 4) DEFAULT NULL, ADD pris2012 NUMERIC(10, 4) DEFAULT NULL, ADD pris2013 NUMERIC(10, 4) DEFAULT NULL, ADD pris2014 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2009 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2010 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2011 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2012 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2013 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2014 NUMERIC(10, 4) DEFAULT NULL');
         $this->addSql('ALTER TABLE Forsyningsvaerk_audit ADD pris2009 NUMERIC(10, 4) DEFAULT NULL, ADD pris2010 NUMERIC(10, 4) DEFAULT NULL, ADD pris2011 NUMERIC(10, 4) DEFAULT NULL, ADD pris2012 NUMERIC(10, 4) DEFAULT NULL, ADD pris2013 NUMERIC(10, 4) DEFAULT NULL, ADD pris2014 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2009 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2010 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2011 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2012 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2013 NUMERIC(10, 4) DEFAULT NULL, ADD co2y2014 NUMERIC(10, 4) DEFAULT NULL');
+        $this->addSql('ALTER TABLE Rapport ADD BaselineCO2El DOUBLE PRECISION DEFAULT NULL, ADD BaselineCO2Varme DOUBLE PRECISION DEFAULT NULL, ADD BaselineCO2Samlet DOUBLE PRECISION DEFAULT NULL, ADD co2BesparelseElFaktor DOUBLE PRECISION DEFAULT NULL, ADD co2BesparelseVarmeFaktor DOUBLE PRECISION DEFAULT NULL, ADD co2BesparelseSamletFaktor DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE Rapport_audit ADD BaselineCO2El DOUBLE PRECISION DEFAULT NULL, ADD BaselineCO2Varme DOUBLE PRECISION DEFAULT NULL, ADD BaselineCO2Samlet DOUBLE PRECISION DEFAULT NULL, ADD co2BesparelseElFaktor DOUBLE PRECISION DEFAULT NULL, ADD co2BesparelseVarmeFaktor DOUBLE PRECISION DEFAULT NULL, ADD co2BesparelseSamletFaktor DOUBLE PRECISION DEFAULT NULL');
     }
 
     /**
@@ -32,5 +34,7 @@ class Version20160222142823 extends AbstractMigration
 
         $this->addSql('ALTER TABLE Forsyningsvaerk DROP pris2009, DROP pris2010, DROP pris2011, DROP pris2012, DROP pris2013, DROP pris2014, DROP co2y2009, DROP co2y2010, DROP co2y2011, DROP co2y2012, DROP co2y2013, DROP co2y2014');
         $this->addSql('ALTER TABLE Forsyningsvaerk_audit DROP pris2009, DROP pris2010, DROP pris2011, DROP pris2012, DROP pris2013, DROP pris2014, DROP co2y2009, DROP co2y2010, DROP co2y2011, DROP co2y2012, DROP co2y2013, DROP co2y2014');
+        $this->addSql('ALTER TABLE Rapport DROP BaselineCO2El, DROP BaselineCO2Varme, DROP BaselineCO2Samlet, DROP co2BesparelseElFaktor, DROP co2BesparelseVarmeFaktor, DROP co2BesparelseSamletFaktor');
+        $this->addSql('ALTER TABLE Rapport_audit DROP BaselineCO2El, DROP BaselineCO2Varme, DROP BaselineCO2Samlet, DROP co2BesparelseElFaktor, DROP co2BesparelseVarmeFaktor, DROP co2BesparelseSamletFaktor');
     }
 }
