b454179337127ddb3c214923240105ad83beef00
Ture Gjørup <ture@ignatz.dk>
2016-03-14 17:26:54 +0100

    AAPLUS-470: Fixed breadcrumbs for Pumpe. Updated templates

---
+++ src/AppBundle/Form/PumpeType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PumpeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nuvaerendeType')
+            ->add('byggemaal')
+            ->add('tilslutning')
+            ->add('indst')
+            ->add('forbrug')
+            ->add('q')
+            ->add('h')
+            ->add('aarsforbrug')
+            ->add('nyPumpe')
+            ->add('nyByggemaal')
+            ->add('nyTilslutning')
+            ->add('vvsnr')
+            ->add('nytAarsforbrug')
+            ->add('elbesparelse')
+            ->add('udligningssaet')
+            ->add('kommentarer')
+            ->add('standInvestering')
+            ->add('fabrikant')
+            ->add('roerlaengde')
+            ->add('roerstoerrelse')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Pumpe'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_pumpe';
+    }
+}
