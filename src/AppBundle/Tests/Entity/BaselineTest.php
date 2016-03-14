8b8a970c8d888a34a7b93b02032908423956a6fb
Troels Ugilt Jensen <tujensen@gmail.com>
2016-03-14 11:20:19 +0100

    Issue #AAPLUS-471 by tuj: Added test cases

--- src/AppBundle/Tests/Entity/BaselineTest.php
+++ src/AppBundle/Tests/Entity/BaselineTest.php
@@ -5,7 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use AppBundle\Entity\Baseline;
 
 class BaselineTest extends KernelTestCase {
-  public function testCalculate() {
+  public function testCalculateElPrimaer() {
     $baseline = new Baseline();
     $baseline->setArealTilNoegletalsanalyse(96.0);
     $baseline->setElForbrugsdataPrimaer1Forbrug(32.0);
@@ -13,5 +13,57 @@ class BaselineTest extends KernelTestCase {
     $baseline->calculate();
     $this->assertEquals(48.0, $baseline->getElForbrugsdataPrimaerGennemsnit());
     $this->assertEquals(0.5, $baseline->getElForbrugsdataPrimaerNoegetal());
+  }
+
+  public function testCalculateElPrimaerNullAreal() {
+    $baseline = new Baseline();
+    $baseline->setArealTilNoegletalsanalyse(null);
+    $baseline->setElForbrugsdataPrimaer1Forbrug(32.0);
+    $baseline->setElForbrugsdataPrimaer2Forbrug(64.0);
+    $baseline->calculate();
+    $this->assertEquals(48.0, $baseline->getElForbrugsdataPrimaerGennemsnit());
+    $this->assertEquals(null, $baseline->getElForbrugsdataPrimaerNoegetal());
+  }
+
+  public function testCalculateElSekundaer() {
+    $baseline = new Baseline();
+    $baseline->setArealTilNoegletalsanalyse(96.0);
+    $baseline->setElForbrugsdataSekundaer1Forbrug(32.0);
+    $baseline->setElForbrugsdataSekundaer3Forbrug(64.0);
+    $baseline->calculate();
+    $this->assertEquals(48.0, $baseline->getElForbrugsdataSekundaerGennemsnit());
+    $this->assertEquals(0.5, $baseline->getElForbrugsdataSekundaerNoegetal());
+  }
+
+  public function testCalculateElSekundaerNullAreal() {
+    $baseline = new Baseline();
+    $baseline->setArealTilNoegletalsanalyse(null);
+    $baseline->setElForbrugsdataSekundaer1Forbrug(32.0);
+    $baseline->setElForbrugsdataSekundaer2Forbrug(64.0);
+    $baseline->calculate();
+    $this->assertEquals(48.0, $baseline->getElForbrugsdataSekundaerGennemsnit());
+    $this->assertEquals(null, $baseline->getElForbrugsdataSekundaerNoegetal());
+  }
+
+  public function testCalculateElBaselineNoegletalForEjendom() {
+    $baseline = new Baseline();
+    $baseline->setArealTilNoegletalsanalyse(160.0);
+    $baseline->setElBaselineFastsatForEjendom(80.0);
+    $baseline->calculate();
+    $this->assertEquals(0.5, $baseline->getElBaselineNoegletalForEjendom());
+  }
+
+  public function testCalculateElBaselineNoegletalForEjendomNullAreal() {
+    $baseline = new Baseline();
+    $baseline->setElBaselineFastsatForEjendom(80.0);
+    $baseline->calculate();
+    $this->assertEquals(null, $baseline->getElBaselineNoegletalForEjendom());
+  }
+
+  public function testCalculateElBaselineNoegletalForEjendomNullBaseline() {
+    $baseline = new Baseline();
+    $baseline->setArealTilNoegletalsanalyse(160.0);
+    $baseline->calculate();
+    $this->assertEquals(null, $baseline->getElBaselineNoegletalForEjendom());
   }
 }
