78c119469922bc6e8442a6ed4e5162f7043983b2
Ture Gjørup <ture@ignatz.dk>
2016-04-05 11:21:30 +0200

    AAPLUS-537: Added migration to move move data

--- app/DoctrineMigrations/Version20160405100941.php
+++ app/DoctrineMigrations/Version20160405100941.php
@@ -16,8 +16,11 @@ class Version20160405100941 extends AbstractMigration {
     // this up() migration is auto-generated, please modify it to your needs
     $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-    $this->addSql('ALTER TABLE Tiltag change column anlaegsinvesteringExRisiko anlaegsinvesteringExRisiko DOUBLE after reelAnlaegsinvestering;');
-    $this->addSql('ALTER TABLE Tiltag change column anlaegsinvestering_beregnet anlaegsinvestering_beregnet DOUBLE after anlaegsinvesteringExRisiko;');
+    //Alter column order
+    $this->addSql('ALTER TABLE Tiltag change column anlaegsinvesteringExRisiko anlaegsinvesteringExRisiko DOUBLE after reelAnlaegsinvestering');
+    $this->addSql('ALTER TABLE Tiltag change column anlaegsinvestering_beregnet anlaegsinvestering_beregnet DOUBLE after anlaegsinvesteringExRisiko');
+
+    $this->addSql("UPDATE Tiltag SET anlaegsinvesteringExRisiko = anlaegsinvestering WHERE discr = 'special' AND anlaegsinvesteringExRisiko IS NULL");
   }
 
   /**
