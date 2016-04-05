25de68191d6e1a5121458d8d9423884b489a82b0
Mikkel Ricky <rimi@aarhus.dk>
2016-04-05 10:24:33 +0200

    Issue #AAPLUS-526 by rimi-itk: Added field "glasandel" to VindueTiltagDetail

---
+++ app/DoctrineMigrations/Version20160405101403.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160405101403 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE TiltagDetail ADD glasandel NUMERIC(10, 4) DEFAULT NULL');
+        $this->addSql('ALTER TABLE TiltagDetail_audit ADD glasandel NUMERIC(10, 4) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE TiltagDetail DROP glasandel');
+        $this->addSql('ALTER TABLE TiltagDetail_audit DROP glasandel');
+    }
+}
