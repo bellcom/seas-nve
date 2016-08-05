8f84c62f1947b5caab7851a18c8a66d726abbec8
turegjorup <ture@ignatz.dk>
2016-08-05 14:38:47 +0200

    Revert "AAPLUS-617: Composer update / Updated knp_paginator"
    This reverts commit f8139e36a003a5d7899e1ab1389a7864f111bef7.

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -7,19 +7,16 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
-use Doctrine\ORM\Query;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Yavin\Symfony\Controller\InitControllerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Form\Type\BygningDashboardType;
-
+use AppBundle\DBAL\Types\BygningStatusType;
 /**
  * Class DashboardController
  * @package AppBundle\Controller
  */
 class DashboardController extends BaseController {
-
   /**
    * @TODO: Missing description.
    *
@@ -32,48 +29,26 @@ class DashboardController extends BaseController {
     $paginator = $this->get('knp_paginator');
 
     if ($this->isGranted('ROLE_ADMIN')) {
+      $finished_buildings_q = $em->getRepository('AppBundle:Rapport')->getByUserAndStatus($user, BygningStatusType::AFLEVERET_RAADGIVER, TRUE);
 
-      // Bygning
-
-      // initialize a query builder
-      $filterBuilder = $this->get('doctrine.orm.entity_manager')
-        ->getRepository('AppBundle:Bygning')
-        ->createQueryBuilder('b ');
-//      $filterBuilder->leftJoin('b.rapport', 'r');
-      $filterBuilder->leftJoin('b.rapport', 'r');
-      $filterBuilder->andWhere('b.aaplusAnsvarlig = :aaplusAnsvarlig');
-      $filterBuilder->setParameter('aaplusAnsvarlig', $user);
-
-      $form = $this->get('form.factory')->create(new BygningDashboardType($this->getDoctrine()), NULL, array(
-        'action' => $this->generateUrl('dashboard'),
-        'method' => 'GET',
-      ));
-
-      if ($request->query->has($form->getName())) {
-        // manually bind values from the request
-        $form->submit($request->query->get($form->getName()));
-
-        // build the query from the given form object
-        $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $filterBuilder);
-      }
-
-      $query = $filterBuilder->getQuery();
-
-      $pagination = $paginator->paginate(
-        $query,
-        $request->query->get('page', 1) /*page number*/,
-        20, /*limit per page*/
-        array('defaultSortFieldName' => 'b.updatedAt', 'defaultSortDirection' => 'desc')
+      $byg_pagination = $paginator->paginate(
+        $finished_buildings_q,
+        $request->query->get('page', 1),
+        10
       );
 
-      return $this->render('AppBundle:Dashboard:admin2.html.twig', array(
-        'pagination' => $pagination,
-        'form' => $form->createView(),
-        'tab' => 'bygninger',
-        'bygninger' => $user->hasGroup('Aa+'),
-        'segmenter' => !$user->getSegmenter()->isEmpty(),
-        'projektleder' => $user->hasGroup('Projektleder'),
-      ));
+      $segmenter_byg_pagination = array();
+      foreach($user->getSegmenter() as $segment) {
+        $seg_query = $em->getRepository('AppBundle:Rapport')->getBySegment($segment);
+
+        $segmenter_byg_pagination[$segment->getId()] = $paginator->paginate(
+          $seg_query,
+          $request->query->get('page', 1),
+          10
+        );
+      }
+
+      return $this->render('AppBundle:Dashboard:admin.html.twig', array('byg_pagination' => $byg_pagination, 'segmenter' => $user->getSegmenter(), 'segmenter_byg_pagination' => $segmenter_byg_pagination));
 
     } else if ($this->isGranted('ROLE_EDIT')) {
 
@@ -100,133 +75,6 @@ class DashboardController extends BaseController {
     } else {
 
       return $this->render('AppBundle:Dashboard:default.html.twig');
-
-    }
-
-  }
-
-  /**
-   * @TODO: Missing description.
-   *
-   * @Route("/segmenter", name="dashboard_segmenter")
-   * @Template()
-   */
-  public function indexSegmenterAction(Request $request) {
-    $user = $this->get('security.context')->getToken()->getUser();
-    $em = $this->getDoctrine()->getManager();
-    $paginator = $this->get('knp_paginator');
-
-    if ($this->isGranted('ROLE_ADMIN') && !$user->getSegmenter()->isEmpty()) {
-
-      // SEGMENTER
-
-      // initialize a query builder
-      $filterBuilder = $this->get('doctrine.orm.entity_manager')
-        ->getRepository('AppBundle:Bygning')
-        ->createQueryBuilder('b ');
-//      $filterBuilder->leftJoin('b.rapport', 'r');
-      $filterBuilder->leftJoin('b.rapport', 'r');
-//      $sFilterBuilder->andWhere('b.aaplusAnsvarlig = :aaplusAnsvarlig');
-      $filterBuilder->andWhere('b.segment IN (:segmenter)');
-      $filterBuilder->setParameter('segmenter', $user->getSegmenter());
-
-      $form = $this->get('form.factory')->create(new BygningDashboardType($this->getDoctrine()), NULL, array(
-        'action' => $this->generateUrl('dashboard_segmenter'),
-        'method' => 'GET',
-      ));
-
-      if ($request->query->has($form->getName())) {
-        // manually bind values from the request
-        $form->submit($request->query->get($form->getName()));
-
-        // build the query from the given form object
-        $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $filterBuilder);
-      }
-
-      $query = $filterBuilder->getQuery();
-
-      $pagination = $paginator->paginate(
-        $query,
-        $request->query->get('page', 1) /*page number*/,
-        20, /*limit per page*/
-        array('defaultSortFieldName' => 'b.updatedAt', 'defaultSortDirection' => 'desc')
-      );
-
-      return $this->render('AppBundle:Dashboard:admin2.html.twig', array(
-        'pagination' => $pagination,
-        'form' => $form->createView(),
-        'tab' => 'segmenter',
-        'bygninger' => $user->hasGroup('Aa+'),
-        'segmenter' => !$user->getSegmenter()->isEmpty(),
-        'projektleder' => $user->hasGroup('Projektleder'),
-      ));
-
-    } else {
-
-      return $this->redirectToRoute('dashboard');
-
-    }
-
-  }
-
-  /**
-   * @TODO: Missing description.
-   *
-   * @Route("/projektleder", name="dashboard_projektleder")
-   * @Template()
-   */
-  public function indexProjektlederAction(Request $request) {
-    $user = $this->get('security.context')->getToken()->getUser();
-    $em = $this->getDoctrine()->getManager();
-    $paginator = $this->get('knp_paginator');
-
-    if ($this->isGranted('ROLE_ADMIN') && !$user->getSegmenter()->isEmpty()) {
-
-      // SEGMENTER
-
-      // initialize a query builder
-      $filterBuilder = $this->get('doctrine.orm.entity_manager')
-        ->getRepository('AppBundle:Bygning')
-        ->createQueryBuilder('b ');
-//      $filterBuilder->leftJoin('b.rapport', 'r');
-      $filterBuilder->leftJoin('b.rapport', 'r');
-      $filterBuilder->andWhere('b.projektleder = :projektleder');
-      $filterBuilder->setParameter('projektleder', $user);
-
-      $form = $this->get('form.factory')->create(new BygningDashboardType($this->getDoctrine()), NULL, array(
-        'action' => $this->generateUrl('dashboard_segmenter'),
-        'method' => 'GET',
-      ));
-
-      if ($request->query->has($form->getName())) {
-        // manually bind values from the request
-        $form->submit($request->query->get($form->getName()));
-
-        // build the query from the given form object
-        $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $filterBuilder);
-      }
-
-      $query = $filterBuilder->getQuery();
-
-      $pagination = $paginator->paginate(
-        $query,
-        $request->query->get('page', 1) /*page number*/,
-        20, /*limit per page*/
-        array('defaultSortFieldName' => 'b.updatedAt', 'defaultSortDirection' => 'desc')
-      );
-
-      return $this->render('AppBundle:Dashboard:admin2.html.twig', array(
-        'pagination' => $pagination,
-        'form' => $form->createView(),
-        'tab' => 'projektleder',
-        'bygninger' => $user->hasGroup('Aa+'),
-        'segmenter' => !$user->getSegmenter()->isEmpty(),
-        'projektleder' => $user->hasGroup('Projektleder'),
-      ));
-
-    } else {
-
-      return $this->redirectToRoute('dashboard');
 
     }
 
