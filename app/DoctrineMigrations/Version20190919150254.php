cf45215efafbc2ea81ee9be9d31f4cd39de85524
Andriy Iun <andriy.yun@gmail.com>
2019-09-19 17:13:22 +0200

    Fixed default values for titlag fields

---
+++ app/DoctrineMigrations/Version20190919150254.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190919150254 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Tiltag CHANGE forbrugFoer forbrugFoer INT DEFAULT NULL, CHANGE forbrugEfter forbrugEfter INT DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Tiltag CHANGE forbrugFoer forbrugFoer INT NOT NULL, CHANGE forbrugEfter forbrugEfter INT NOT NULL');
+    }
+}
