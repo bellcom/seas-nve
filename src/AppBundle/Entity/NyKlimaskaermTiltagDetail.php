1f430a4ee83508a9da780a20adafa25b4ca41934
Andriy Iun <andriy.yun@gmail.com>
2020-06-17 16:04:50 +0200

    S5-229 Added adjustments for Nyklimaskaerm detail t Inde/Ude fields

--- src/AppBundle/Entity/NyKlimaskaermTiltagDetail.php
+++ src/AppBundle/Entity/NyKlimaskaermTiltagDetail.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use AppBundle\DBAL\Types\MonthType;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Mapping as ORM;
 use AppBundle\Annotations\Calculated;
 use AppBundle\DBAL\Types\CardinalDirectionType;
@@ -15,6 +16,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Table()
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
  */
 class NyKlimaskaermTiltagDetail extends KlimaskaermTiltagDetail
 {
@@ -344,9 +346,6 @@ class NyKlimaskaermTiltagDetail extends KlimaskaermTiltagDetail
      * @inheritDoc
      */
     public function getTIndeC() {
-        if ($this->tIndeDetailed && !empty($this->getTIndeMonthly())) {
-            return array_sum($this->getTIndeMonthly()) / count($this->getTIndeMonthly());
-        }
         return $this->tIndeC;
     }
 
@@ -354,9 +353,6 @@ class NyKlimaskaermTiltagDetail extends KlimaskaermTiltagDetail
      * @inheritDoc
      */
     public function getTUdeC() {
-        if ($this->tUdeDetailed && !empty($this->getTUdeMonthly())) {
-            return array_sum($this->getTUdeMonthly()) / count($this->getTUdeMonthly());
-        }
         return $this->tUdeC;
     }
 
@@ -426,6 +422,22 @@ class NyKlimaskaermTiltagDetail extends KlimaskaermTiltagDetail
     public function calculate() {
         $this->tOpvarmningTimerAar = $this->calculateTOpvarmningTimerAar();
         parent::calculate();
+    }
+
+    /**
+     * PreUpdate handler.
+     *
+     * @ORM\PreUpdate
+     * @param \Doctrine\ORM\Event\LifecycleEventArgs $event
+     */
+    public function preUpdate(LifecycleEventArgs $event) {
+        if ($this->tIndeDetailed && !empty($this->getTIndeMonthly())) {
+             $this->tIndeC = array_sum($this->getTIndeMonthly()) / count($this->getTIndeMonthly());
+        }
+
+        if ($this->tUdeDetailed && !empty($this->getTUdeMonthly())) {
+            $this->tUdeC = array_sum($this->getTUdeMonthly()) / count($this->getTUdeMonthly());
+        }
     }
 
 }
